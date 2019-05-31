4297e0e63ea7278ea311bf4fc8364159cfc0e587
Rayyan Hussain <rayyanhussain220@gmail.com>
2019-05-30 22:54:32 -0700

    made style edits

--- header.inc.php
+++ header.inc.php
@@ -7,10 +7,16 @@
  */
 ?>
 <!-- This is a common navigation base to call different web pages on the website -->
-<div class="topnav">
+<!-- <div class="topnav">
     <a href="index.php">Home</a>
     <a href="list_customers.php">List Customers</a>
     <a href="list_products.php">List Products</a>
 </div></div>
-<h1>Missing Children Database</h1>
+<h1>Missing Children Database</h1> -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<div class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand text-white">Missing Children Database</a>
+  <a href = "index.php" class="navbar-brand">Home</a>
+</div>
 
--- index.php
+++ index.php
@@ -9,14 +9,23 @@
 <head>
     <title>Missing Children - Missing Child DB</title>
     <link rel="stylesheet" href="base.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 <?php
 require_once 'header.inc.php';
 ?>
-<div>
-    <h2>About</h2>
-    <div>This is sample PHP program that does simple database operations calling the mysqli library in PHP</div>
+<div class = "container paddingAdd">
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Missing Children Database</h1>
+    <p class="lead">This is an online repository of missing children. You can see all missing children, search based on state, and more.</p>
+    <p>This is <strong>NOT</strong> a place to report missing children. For that, please call 911.</p>
+  </div>
+</div>
+    <h2>Available commands: </h2>
+    <br>
+    <button type="button" class="btn btn-dark"><a class = "text-white"href="list_customers.php">List <em>all</em> missing children</a></button>
 </div>
 </body>
 </html>
