567a7c11042c004a6f7e6131e1bd78c686aee9ec
Rayyan Hussain <rayyanhussain220@gmail.com>
2019-05-30 17:24:42 -0700

    added files to repo

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markk@uw.edu
+ * Date: 7/24/2018
+ * Time: 2:45 PM
+*/?>
+<html>
+<head>
+    <title>Missing Children - Missing Child DB</title>
+    <link rel="stylesheet" href="base.css">
+</head>
+<body>
+<?php
+require_once 'header.inc.php';
+?>
+<div>
+    <h2>About</h2>
+    <div>This is sample PHP program that does simple database operations calling the mysqli library in PHP</div>
+</div>
+</body>
+</html>
