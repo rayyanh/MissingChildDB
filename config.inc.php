80ecfc7ceb1b53b6887699d581552ce500f2b2be
Rayyan Hussain <rayyanhussain220@gmail.com>
2019-06-12 19:28:30 -0700

    Deleting config
    Security reasons

--- config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @Author Rayyan Hussain
- */
-$servername = 'loremipsum'; // Server
-$username = 'loremipsum';
-$password = 'loremipsum';
-$database = 'loremipsum';
-$port = 0000; // Port
